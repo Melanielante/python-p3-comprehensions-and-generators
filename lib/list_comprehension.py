a83da8b132fc0d37b5c8f2f69a79bbda17389657
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-08-20 13:14:38 +0300

    lc and ge

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    # list = range(0,8)
+    return  [n for n in num_list if n % 2 == 0]
+    
     pass
 
 def make_exclamation(sentence_list):
+    return [sentence_list + "!" for sentence_list in sentence_list]
     pass
